52ddb9d1576b73c2f35d39b4f84eda765a229e93
Philipp Gayret <philipp@skillable.eu>
2013-11-12 21:44:10 +0100

    Create store.js
    Copy paste :D

---
+++ store.js
@@ -0,0 +1,104 @@
+/**
+ * A storage service that persists to localStorage, maintains an internal cache as well, to avoid large JSON parsing on .get calls.
+ * Provides utilities to watch for changes, synchronizes cross-window and can even two-way-bind objects in Store with objects on a $scope.
+ */
+app.service("Store", ["$window", function ($window) {
+
+	var that = this;
+	var cache = {};
+	var watched = {};
+
+	this.get = function(key) {
+		if(cache.hasOwnProperty(key)){
+			return cache[key];
+		}
+		else{
+			var result = localStorage.getItem(key);
+			return result == null ? result : angular.fromJson(result);
+		}
+	};
+
+	/**
+	 * Checks if we have the object, less expensive than actually going there and maybe even having to parse it.
+	 */
+	this.has = function(key){
+		return cache.hasOwnProperty(key) || localStorage.hasOwnProperty(key);
+	};
+
+	this.set = function(key, value) {
+		if(value != undefined){
+			cache[key] = value;
+			localStorage.setItem(key, angular.toJson(value));
+		}
+	};
+
+	/**
+	 * Watches an item for changes, when they happen the given callback will be called.
+	 * Note that the callback will not be $scope.$apply'd.
+	 */
+	this.watch = function(key, callback){
+		watched[key] = ([]||watched[key]);
+		watched[key].push(callback);
+	};
+
+	angular.element($window).bind("storage", function(event){
+		cache[event.key] = event.newValue;
+		if(watched.hasOwnProperty(event.key)){
+			var value = angular.fromJson(event.newValue);
+			for(i in watched[event.key]){
+				watched[event.key][i](value);
+			}
+		}
+	});
+
+	/**
+	 * Provides a two-way-binding between localStorage and a $scope object.
+	 * Uses $watch to compare for equality, not reference.
+	 * @param storeKey is optional, defaults to given scopeKey
+	 */
+	this.mirror = function($scope, scopeKey, storeKey){
+
+		storeKey = storeKey || scopeKey;
+
+		// we the reference to this callback on delete
+		var callback = function(value){
+			$scope[scopeKey] = value;
+			$scope.$apply();
+		};
+
+		// add it to watched so changes in localStorage go to $scope
+		watched[storeKey] = watched[storeKey] || [];
+		watched[storeKey].push(callback);
+
+		// when the scope is destroyed, clean up watched
+		$scope.$on("$destroy", function() {
+			var index = watched[storeKey].indexOf(callback);
+			watched[storeKey].splice(index, 1);
+			if(watched[storeKey].length == 0){
+				delete watched[storeKey];
+			}
+        });
+
+		// watch for changes from $scope
+		$scope.$watch(scopeKey, function(newValue, oldValue){
+			that.set(storeKey, newValue);
+		}, true);
+
+		// initialize $scope when $scope has no value and Store does
+		if($scope[scopeKey] == null && that.has(storeKey)){
+			$scope[scopeKey] = that.get(storeKey);
+		}
+
+		// initialize Store when $store has no value and $scope does
+		if(!that.has(storeKey) && $scope[scopeKey] == null){
+			that.set(storeKey, $scope[scopeKey]);
+		}
+
+		// return either
+		return $scope[scopeKey] || that.get(storeKey);
+
+	};
+
+    return this;
+
+}]);
